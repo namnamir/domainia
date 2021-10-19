d47107401bd633c5191a9755eaddadd1a4336c83
Ali N <namnamir@users.noreply.github.com>
2021-10-19 10:20:56 +0200

    Add comment

--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,4 @@
+# get the API from here: https://whois.whoisxmlapi.com/documentation/making-requests
 api_key_whoisxml = ''
+# get the API from here: https://docs.nameauditor.com/
 api_key_nameauditor = ''
