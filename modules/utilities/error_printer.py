d3ca18b5903552ce02baaa4af0f53eabe50b5b27
Ali N <namnamir@gmail.com>
2024-01-25 13:26:01 +0100

    fix comments + make it more readable

--- modules/utilities/error_printer.py
+++ modules/utilities/error_printer.py
@@ -17,56 +17,56 @@ def error_printer(flag, texts):
         texts (list): a list contains text for each verbosity in the following format
                       ["text for verbosity 1", ..., "text for verbosity 5"]
     """
-    verbosity_1 = '      │        ├─1─■ '
-    verbosity_2 = '      │        ├─2─■ '
-    verbosity_3 = '      │        ├─3─■ '
-    verbosity_4 = '      │        ├─4─■ '
-    verbosity_5 = '      │        ├─5─■ '
-
-    # if flag is True/exception and verbosity is not 0 (or False)
+    # If flag is True/exception and verbosity is not 0 (or False)
     if flag and config['verbosity']:
-        # print error messages for verbosity 1 (-v)
+        # Print error messages for verbosity 1 (-v)
         if (
             config['verbosity'] >= 1
             and len(texts) >= 1
             and texts[0]
         ):
-            printer(f'{verbosity_1}{Fore.RED}{texts[0]}{Style.RESET_ALL}')
+            verbosity = '      │        ├─1─■ '
+            printer(f'{verbosity}{Fore.RED}{texts[0]}{Style.RESET_ALL}')
 
-        # print error messages for verbosity 2 (-vv)
+        # Print error messages for verbosity 2 (-vv)
         if (
             config['verbosity'] >= 2
             and len(texts) >= 2
             and texts[1]
         ):
-            printer(f'{verbosity_2}{Fore.RED}{texts[1]}{Style.RESET_ALL}')
+            verbosity = '      │        ├─2─■ '
+            printer(f'{verbosity}{Fore.RED}{texts[1]}{Style.RESET_ALL}')
 
-        # print error messages for verbosity 3 (-vvv)
+        # Print error messages for verbosity 3 (-vvv)
         if (
             config['verbosity'] >= 3
             and len(texts) >= 3
             and texts[2]
         ):
-            printer(f'{verbosity_3}{Fore.RED}{texts[2]}{Style.RESET_ALL}')
+            verbosity = '      │        ├─3─■ '
+            printer(f'{verbosity}{Fore.RED}{texts[2]}{Style.RESET_ALL}')
 
-        # print error messages for verbosity 4 (-vvvv)
+        # Print error messages for verbosity 4 (-vvvv)
         if (
             config['verbosity'] >= 4
             and len(texts) >= 4
             and texts[3]
         ):
-            printer(f'{verbosity_4}{Fore.RED}{texts[3]}{Style.RESET_ALL}')
+            verbosity = '      │        ├─4─■ '
+            printer(f'{verbosity}{Fore.RED}{texts[3]}{Style.RESET_ALL}')
 
-        # print error messages for verbosity 5 (-vvvvv)
+        # Print error messages for verbosity 5 (-vvvvv)
         if config['verbosity'] >= 5:
             if (
                 len(texts) >= 5
                 and texts[4]
             ):
-                printer(f'{verbosity_5}{Fore.RED}{texts[4]}{Style.RESET_ALL}')
+                verbosity = '      │        ├─5─■ '
+                printer(f'{verbosity}{Fore.RED}{texts[4]}{Style.RESET_ALL}')
 
-            # in case, it is an exception error message
+            # In case, it is an exception error message
             if flag == 'exception':
                 ex = exception_details()
-                printer(f'{verbosity_5}Exception Message: {Fore.MAGENTA}{ex[0]} ➜ '
+                verbosity = '      │        ├─5─■ '
+                printer(f'{verbosity}Exception Message: {Fore.MAGENTA}{ex[0]} ➜ '
                         f'{ex[4]}:{ex[3]}{Fore.RED} == {ex[2]} ⚊ {ex[1]}{Style.RESET_ALL}')
